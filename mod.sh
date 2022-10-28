bcfa8cc85b7653abaf9cfd53b2f71ddf6a1dace0
denx24x <d.belozorov1@gmail.com>
2022-10-29 01:20:35 +0300

    +mod

---
+++ mod.sh
@@ -0,0 +1,17 @@
+input="$1"
+while read -r line
+do
+	group=$(echo "$line" | awk '{print $NF}')
+	if [[ ${group:1:1} == "3" ]]
+	then
+		num=${group:2:1}
+	else
+		if [[ ${group:2:1} == "1" ]]
+		then
+			num="5"
+		else
+			num="6"
+		fi
+	fi
+	echo "$line" | awk -v num=$num '$NF=num'
+done < "$input"
